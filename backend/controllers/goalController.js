51a50ed632a941ba5d43a86c27f91dc941d43551
Pavel Smirnov <smrnvpvl@gmail.com>
2022-10-16 10:17:46 +0300

    feat: added user model, controller, routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
 // @desc Get goals
 // @route GET/api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "GET GOALS" });
+  const goals = await Goal.find({});
+  res.status(200).json(goals);
 });
 
 // @desc Set goal
@@ -15,22 +17,42 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  console.log(req.body);
-  res.status(200).json({ message: "SET GOAL" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 // @desc Update goal
 // @route PUT/api/goal/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `UPDATE GOAL ${req.params.id} ` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({ updatedGoal });
 });
 
 // @desc Delete goal
 // @route DELETE/api/goal/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `DELETE GOAL ${req.params.id} ` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+// @desc Register new user
+// @route POST /api/users
+// @access Public
+const registerUser = (req, res) => {
+  res.json({ message: "reg ister user" });
+};
+
+// @desc Authenticate a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = (req, res) => {
+  res.json({ message: "login user" });
+};
+
+// @desc Get user data
+// @route GET /api/users/me
+// @access Public
+const getMe = (req, res) => {
+  res.json({ message: "user data display" });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
