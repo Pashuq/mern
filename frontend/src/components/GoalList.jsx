61db1a3127e6e7c8c5b9a7c19a8032dbf19295e6
Pavel Smirnov <smrnvpvl@gmail.com>
2022-12-15 22:04:22 +0300

    fix: goal model fix backend

--- frontend/src/components/GoalList.jsx
+++ frontend/src/components/GoalList.jsx
@@ -1,10 +1,34 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { AiTwotoneDelete } from "react-icons/ai";
+import { FaPencilAlt } from "react-icons/fa";
+import { deleteGoal } from "../features/goals/goalSlice";
+
 function GoalList() {
+  const dispatch = useDispatch();
   const { goals } = useSelector((state) => state.goal);
+
+  const handleDeleteGoal = (id) => {
+    dispatch(deleteGoal(id));
+  };
+
+  const handleUpdateGoal = () => {};
+
   return (
     <div>
       {goals.map((goal) => (
-        <p key={goal._id}>{goal.text}</p>
+        <div key={goal._id}>
+          <div style={{ display: "flex" }}>
+            <AiTwotoneDelete
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                handleDeleteGoal(goal._id);
+              }}
+            />
+            <FaPencilAlt style={{ cursor: "pointer" }} />
+          </div>
+          {goal.text}
+        </div>
       ))}
     </div>
   );
